96749d5feda40e25aa5da63fb83a7b9e21490abb
d-Wine <dy98wine@gmail.com>
2020-11-25 09:49:51 +0630

    gofmt datatype

---
+++ go-basics/02-Datatypes-Variables/datatypes.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main()  {
+	//integer
+	var i int =  34
+	fmt.Println(i)
+	//string
+	var s string = "Hello World!"
+	fmt.Println(s)
+	//float
+	var f float64 = 3.14159265359
+	fmt.Println(f)
+	//boolean
+	var b bool = true
+	fmt.Println(b)
+}
